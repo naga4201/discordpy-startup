c75f4e2c1726acd542db813e91ac44ef1cc1c270
naga4201 <85100010+naga4201@users.noreply.github.com>
2021-05-31 17:18:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,4 +23,10 @@ async def gia(ctx):
     await ctx.send(':ElrV7StVkAEnJXn:')
 
 
+@client.command()
+async def addch(ctx, channel_name):
+    thread_category = bot.get_channel(786837217041055785)  
+    await ctx.guild.create_text_channel(channel_name, category=thread_category)
+    await ctx.send(f"スレッド[ {channel_name} ]を作成しました！")    
+    
 bot.run(token)
